97c92f5a7f2fc0045ce1d13391c298323286cc9d
rachel <rachal@wangxutech.com>
2026-04-16 19:14:24 +0800

    feat: 版本号修改

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 7
-        versionName = "0.0.2"
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
